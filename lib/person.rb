fab7b967cb9ec3ab7ac324bad759cf5b7e2920fa
Elizabeth Nguyen <saper209@yahoo.com>
2022-07-29 23:50:50 -0700

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,76 @@
 # your code goes here
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account
+
+  def initialize(name)
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+    @name = name
+  end
+
+  def happiness=(value)
+    @happiness = if (value < 0)
+      0
+    elsif (value > 10)
+      10
+    else 
+      value
+    end
+  end
+
+  def hygiene=(value)
+    @hygiene = if (value < 0)
+      0
+    elsif (value > 10)
+      10
+    else 
+      value
+    end
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    if (topic === "politics")
+      self.happiness -= 2
+      friend.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif (topic === "weather")
+      self.happiness += 1
+      friend.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+end
